40bcf48a647b609db61ba46d2551e1c37655a9ad
PRIYA9065 <147863167+PRIYA9065@users.noreply.github.com>
2026-07-12 16:55:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+const p = document.querySelector("p");
+const btn = document.querySelector("button");
+btn.addEventListener("click", () => {
+	p.innerHTML = '<h1>Entered Metaverse</h1>';
+})
